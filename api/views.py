44c8772d7509e30366a89e991dcd76a52b4a4a5d
prosper <edwardprosper001@gmail.com>
2023-02-06 21:53:55 +0100

    posts

--- api/views.py
+++ api/views.py
@@ -16,10 +16,65 @@ def post_list(request):
 
 @api_view(["GET"])
 def post_detail(request, slug):
-    if request.method == "GET":
+    try:
         post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
         serializer = PostSerializer(post)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["PUT"])
+def post_update(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "PUT":
+        serializer = PostSerializer(post, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data["success"] = "Post update successful"
+            return Response(data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(["DELETE"])
+def post_delete(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "DELETE":
+        operation = post.delete()
+        data = {}
+        if operation:
+            data["Success"] = "Post has been deleted successfully!"
+        else:
+            data["Failure"] = "Post deletion failed"
+        return Response(data=data)
+
+
+@api_view(["POST"])
+def post_create(request):
+    user = request.user
+    post = Post(author=user)
+    if request.method == "POST":
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
 
 
 
