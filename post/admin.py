c3c0a827d1d59c217ad4a4f000161b7738373d15
prosper <edwardprosper001@gmail.com>
2023-02-06 21:15:42 +0100

    posts

--- post/admin.py
+++ post/admin.py
@@ -4,3 +4,6 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ["title", "author", "date_updated"]
     prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
