1a815a55cc61b57b60bdefb12fee72c683b6f552
prosper <edwardprosper001@gmail.com>
2023-02-06 21:23:35 +0100

    posts

--- MAIN/settings.py
+++ MAIN/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'post.apps.PostConfig'
+    'post.apps.PostConfig',
+    'api.apps.ApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
